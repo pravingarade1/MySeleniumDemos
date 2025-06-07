25daada7a9aee3117c9cbbe97cea95469dd87d0e
Pravin <pravingarade123@gmail.com>
2025-06-07 11:23:46 +0530

    My commit on 7/6/2025

---
+++ MySeleniumAutomationProject/src/com/WebdriverAssignments/A08LocateByAbsulateXpath.java
@@ -0,0 +1,43 @@
+package com.WebdriverAssignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A08LocateByAbsulateXpath {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver(); 
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.echotrak.com/Login.aspx?ReturnUrl=%2f");
+		
+		WebElement username = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/div/div/form/fieldset/div[2]/fieldset/input"));
+		
+	
+		username.sendKeys("pravin");
+		
+		WebElement pass = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/div/div/form/fieldset/div[2]/fieldset/input[2]"));
+		
+		pass.sendKeys("pravin123");
+		
+		WebElement loginbtn = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/div/div/form/fieldset/div[3]/div/input"));
+		
+		loginbtn.click();  
+		
+		WebElement errormsg = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/div/div/form/fieldset/div/span"));
+		
+		String msg=errormsg.getText();
+		
+		System.out.println("Error Message : "+msg);
+		
+		driver.close();
+		
+	
+
+	}
+
+}
---
+++ MySeleniumAutomationProject/src/com/WebdriverAssignments/A09LocateByAbsulateXpathGoogle.java
@@ -0,0 +1,29 @@
+package com.WebdriverAssignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A09LocateByAbsulateXpathGoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com");
+		
+		WebElement searchbox = driver.findElement(By.xpath("/html/body/div/div[3]/form/div/div/div/div/div[2]/textarea"));
+		
+	
+		searchbox.sendKeys("Pattankodoli Yatra");
+		
+		searchbox.sendKeys(Keys.ENTER);
+	
+	}
+
+}
